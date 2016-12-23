4e5c71ad13e47524e8f8129f5ca4d7fe29cb2f8c
Rémi Prévost <remi@exomel.com>
2016-12-23 11:30:34 -0500

    Add bot

---
+++ config/config.exs
@@ -0,0 +1,9 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+use Mix.Config
+
+# Slack
+config :slack, api_token: System.get_env("SLACK_API_TOKEN")
+
+# Leakbot
+config :leakbot, leak_channel: System.get_env("LEAK_CHANNEL")
---
+++ lib/leakbot/bot.ex
@@ -0,0 +1,13 @@
+defmodule Leakbot.Bot do
+  # Inclusions
+  use Slack
+
+  def handle_event(%{channel: "D" <> _, type: "message", text: text}, slack, state) do
+    leak_channel = Application.get_env(:leakbot, :leak_channel);
+    send_message("🚿 #{text}", leak_channel, slack)
+
+    {:ok, state}
+  end
+
+  def handle_event(_message, _slack, state), do: {:ok, state}
+end
