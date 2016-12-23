bbfa1b45d6c13de35deff7eb6d92ae73c54ae999
Rémi Prévost <remi@exomel.com>
2016-12-23 11:37:00 -0500

    Remove silly code

--- lib/leakbot.ex
+++ lib/leakbot.ex
@@ -2,7 +2,6 @@ defmodule Leakbot do
   use Application
 
   def start(_type, _args) do
-    IO.inspect("lol")
     Slack.Bot.start_link(Leakbot.Bot, [], Application.get_env(:slack, :api_token))
   end
 end
